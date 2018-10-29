2c634c5c2fd71dc44d900bbbef0634fc3c2ef19e
LiYanLance <leeyanly@163.com>
2018-10-29 21:49:03 +0800

    add todolist title

--- src/components/App.js
+++ src/components/App.js
@@ -6,6 +6,7 @@ export default class extends Component{
     render(){
         return(
             <div className="container">
+                <h2 style={{"textAlign":"center", "textTransform": "uppercase"}}>todo list</h2>
                 <AddTodo/>
                 <VisibleTodoList/>
                 <Footer/>
